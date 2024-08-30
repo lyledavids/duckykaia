30092922daa2c5e62b7a53ac2881e845dab3e464
Lyle Davids <lyledsa@gmail.com>
2024-08-30 19:00:49 +0200

    .

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 On-chain art creation on Kaia
 
-deployed to kaia testnet
+
+deployed to kaia testnet using remix
 0x05D0AcA3ba12f010f6A26104da5cB83419723842
---
+++ src/ducky.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTArtCreator is ERC721, ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Ducky", "Duck") {}
+
+    function safeMint(address to, string memory uri) public returns (uint256) {
+        uint256 tokenId = _tokenIds.current();
+        _tokenIds.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
